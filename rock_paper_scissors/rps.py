b17813436bc7de46cde1e834acd23f622444e66b
Nicholas Pung <nckpng95@gmail.com>
2019-10-10 13:24:05 -0700

    Finish rps

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -3,7 +3,22 @@
 import sys
 
 def rock_paper_scissors(n):
-  pass 
+  if n == 0:
+    return [[]]
+  elif n == 1:
+    return [['rock'], ['paper'], ['scissors']]
+  else:
+    rps_under = rock_paper_scissors(n-1)
+    base_case = [['rock'], ['paper'], ['scissors']]
+    result = []
+    for i in range(len(rps_under)):
+      a = rps_under[i] + base_case[0]
+      b = rps_under[i] + base_case[1]
+      c = rps_under[i] + base_case[2]
+      result.append(a)
+      result.append(b)
+      result.append(c)
+    return result
 
 
 if __name__ == "__main__":
